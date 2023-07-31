a59a45d27e09d3d9b4942c17c04f300fbe6525de
Mona Lisa <anastasiia.rn@gmail.com>
2023-07-31 14:37:04 -0400

    complete Group By queries

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT count(students.*) AS student_count, cohorts.name as cohort_name
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) >=18
+ORDER BY student_count;
